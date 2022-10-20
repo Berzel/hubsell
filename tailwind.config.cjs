4f685adfe36beb4c8a33853972f5a824cb256f9a
berzel <berzel.best@gmail.com>
2022-10-20 22:20:34 +0200

    Updated tailwind configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+        'sm': '576px',
+        'md': '768px',
+        'lg': '992px',
+        'xl': '1100px',
+    },
+    container: {
+        center: true,
+        padding: '0.75rem',
+    },
     extend: {},
   },
   plugins: [],
